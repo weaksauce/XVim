be520ef47d3315a9f7d973a50100176b36bc5980
Patrick <opensourcepatrick@gmail.com>
2012-11-08 19:19:42 -0800

    removed unnecessary files and modifications.

--- XVim/XVimHookManager.m
+++ XVim/XVimHookManager.m
@@ -28,23 +28,6 @@
 	[IDESourceCodeEditorHook hook];
     //[DVTSourceTextScrollViewHook hook];
     [IDEEditorHook hook];
-//    IDEApplicationController * appdel = [IDEApplicationController sharedAppController];
-    NSMenu* main = [NSApp mainMenu];
-    
-    NSMenu* xvimMenu = [[NSMenu alloc] initWithTitle:@"XVim"];
-    NSMenuItem *xvimMenuitem = [[NSMenuItem alloc] initWithTitle:@"XVim" action:nil keyEquivalent:@""];
-    [xvimMenuitem setSubmenu:xvimMenu];
-    [main addItem:xvimMenuitem];
-    [xvimMenu release];
-    [xvimMenuitem release];
-    
-    xvimMenuitem = [[NSMenuItem allocWithZone:[NSMenu menuZone]] initWithTitle:@"Toggle XVim on/off" action:NULL keyEquivalent:@""];
-    [xvimMenuitem setTag:toggleXVimMenuitemTag];
-    [xvimMenuitem setTarget:[XVim instance]];
-    [xvimMenuitem setAction:@selector(toggleXVim:)];
-    [xvimMenu addItem:xvimMenuitem];
-    [xvimMenuitem release];
-//    [main setSubmenu:xvimMenu forItem:xvimMenuitem];
 }
 
 @end
